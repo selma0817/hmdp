eaca439872a2f97e4327edf525672dc37a76cbc7
selma0817 <selmapan11@gmail.com>
2026-04-25 18:28:17 -0400

    redisson distributed locks

---
+++ hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,29 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+    @Bean
+    public RedissonClient redissonClient2(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6380");
+        return Redisson.create(config);
+    }
+    @Bean
+    public RedissonClient redissonClient3(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6381");
+        return Redisson.create(config);
+    }
+}
--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,6 +10,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private RedissonClient redissonClient;
     @Override
     public Result seckillVoucher(Long voucherId) {
         // 1. search for voucher
@@ -46,8 +50,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
         Long userId = UserHolder.getUser().getId();
         // create lock object
-        SimpleRedisLock lock = new SimpleRedisLock("order:"+userId, stringRedisTemplate);
-        boolean isLock = lock.tryLock(1200);
+        // SimpleRedisLock lock = new SimpleRedisLock("order:"+userId, stringRedisTemplate);
+        RLock lock = redissonClient.getLock("lock:order:"+userId);
+
+        boolean isLock = lock.tryLock();
         if(!isLock){
             // fail to get lock
             return Result.fail("each user can only get single order on each secKill voucher");
