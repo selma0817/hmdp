10ed9814083cb11a84865ddedae13987e99247fa
selma0817 <selmapan11@gmail.com>
2026-04-27 13:59:16 -0400

    blog, like and mutual follow

--- hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,28 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,5 +34,97 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryBlogById(Long id) {
+        // 1. search for blog
+        Blog blog = getById(id);
+        if (blog == null){
+            return Result.fail(" blog not exist");
+        }
+        queryBlogUser(blog);
+        isBlogLiked(blog);
+        return Result.ok(blog);
+    }
+    private void isBlogLiked(Blog blog){
+
+        UserDTO user = UserHolder.getUser();
+        if(user==null){
+            // user not loginn no need to check if liked
+            return;
+        }
+        Long userId = user.getId();
+        String key = RedisConstants.BLOG_LIKED_KEY + blog.getId();
+        // 2. decide if current user has liked or not
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        blog.setIsLike(score!=null);
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        List<Blog> records = page.getRecords();
+        records.forEach(blog -> {
+            this.queryBlogUser(blog);
+            this.isBlogLiked(blog);});
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result likeBlog(Long id) {
+        // 1. get current loggin user
+        Long userId = UserHolder.getUser().getId();
+        String key = RedisConstants.BLOG_LIKED_KEY + id;
+        // 2. decide if current user has liked or not
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        // 3. if haven't like, can like
+        if(score==null){
+            boolean isSuccess = update().setSql("liked=liked+1").eq("id", id).update();
+            if(isSuccess){
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
+            }
+        }else{
+            boolean isSuccess = update().setSql("liked=liked-1").eq("id", id).update();
+            if(isSuccess){
+                stringRedisTemplate.opsForZSet().remove(key, userId.toString());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Result queryBloglikes(Long id) {
+        String key = RedisConstants.BLOG_LIKED_KEY + id;
+        // 1. find top 5 like user, use zrange 0 4
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        if(top5 == null || top5.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+        // 2. get user id
+        List<Long> ids = top5.stream().map(Long::valueOf).collect(Collectors.toList());
+        // 3. use user id to find user
+        String idStr = StrUtil.join(",", ids);
+        List<UserDTO> usersDTOS = userService.query()
+                .in("id", ids).last("ORDER BY FIELD(id," + idStr +")").list()
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        // return userDTO
+        return Result.ok(usersDTOS);
+    }
+
+    private void queryBlogUser(Blog blog){
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
 
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +31,66 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IUserService userService;
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // get current loggin user
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        // 1. decide if follow or unfollow
+        if(isFollow){
+            // 2. if follow add
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);
+            if(isSuccess){
+                // put followed user id into redis
+                stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+
+            }
+        }else{
+            // 3. if unfollow, remove
+            // delete from tb_follow where userId = ? and follow_user_id = ?
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId));
+            // remove followed user from redis
+            if(isSuccess) {
+                stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+            }
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 1. search if follow. select * from tb_follow where user_id = ? and follow_user_id = ?
+        Long userId = UserHolder.getUser().getId();
+        Long count = query().eq("user_id", userId)
+                .eq("follow_user_id", followUserId).count();
+
+        return Result.ok(count>0);
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+
+        // get current user
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:"+userId;
+        String key2 = "follows:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(key, key2);
+        if(intersect == null || intersect.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        List<UserDTO> userDTOS = userService.listByIds(ids)
+                .stream().map(user->BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(userDTOS);
+    }
 }
