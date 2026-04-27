1e672010f752e48ac22ba2fe958638b82cf267bd
selma0817 <selmapan11@gmail.com>
2026-04-26 20:27:31 -0400

    redis stream implemented message queue

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
@@ -13,6 +14,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.stream.*;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
@@ -20,7 +22,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.time.Duration;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -45,14 +50,14 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         SECKILL_SCRIPT.setResultType(Long.class);
     }
 
-    private BlockingQueue<VoucherOrder> orderTasks = new ArrayBlockingQueue<>(1024 * 1024);
+    // private BlockingQueue<VoucherOrder> orderTasks = new ArrayBlockingQueue<>(1024 * 1024);
     private static final ExecutorService SECKILL_ORDER_EXECUTOR = Executors.newSingleThreadExecutor();
 
     @PostConstruct
     private void init() {
         SECKILL_ORDER_EXECUTOR.submit(new VoucherOrderHandler());
     }
-
+    /*
     private class VoucherOrderHandler implements Runnable {
         @Override
         public void run() {
@@ -68,7 +73,74 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
         }
     }
+*/
+    private class VoucherOrderHandler implements Runnable {
+        String queueName = "stream.orders";
+        @Override
+        public void run() {
+            while (true) {
 
+                try {
+                    // 1. get voucher order from message queue fron redis
+                    // XREADGROUP GROUP g1, c1 COUNT 1 BLOCK 2000 STREAMS streams.order
+                    List<MapRecord<String, Object, Object>> list = stringRedisTemplate.opsForStream().read(
+                            Consumer.from("g1", "c1"),
+                            StreamReadOptions.empty().count(1).block(Duration.ofSeconds(2)),
+                            StreamOffset.create(queueName, ReadOffset.lastConsumed())
+                    );
+                    // 2. decide if getting message is susscessful
+                    if (list==null || list.isEmpty()){
+                        // 2.1 if fail, wait for next
+                        continue;
+                    }
+                    // 2.2 if success, can order
+                    MapRecord<String, Object, Object> record = list.get(0);
+                    Map<Object, Object> values = record.getValue();
+                    VoucherOrder voucherOrder = BeanUtil.fillBeanWithMap(values, new VoucherOrder(), true);
+                    handleVoucherOrder(voucherOrder);
+                    // 3. send ACK. stream.orders g1 id
+                    stringRedisTemplate.opsForStream().acknowledge(queueName, "g1", record.getId());
+                } catch (Exception e) {
+                    log.error("dealing with order error", e);
+                    handlePendingList();
+                }
+            }
+
+        }
+
+        private void handlePendingList() {
+            while (true) {
+                try {
+                    // 1. get voucher order from message queue fron redis
+                    // XREADGROUP GROUP g1, c1 COUNT 1 BLOCK 2000 STREAMS streams.order 0
+                    List<MapRecord<String, Object, Object>> list = stringRedisTemplate.opsForStream().read(
+                            Consumer.from("g1", "c1"),
+                            StreamReadOptions.empty().count(1),
+                            StreamOffset.create(queueName, ReadOffset.from("0"))
+                    );
+                    // 2. decide if getting message is susscessful
+                    if (list==null || list.isEmpty()){
+                        // 2.1 if fail, no message is in pending list
+                        break;
+                    }
+                    // 2.2 if success, can order
+                    MapRecord<String, Object, Object> record = list.get(0);
+                    Map<Object, Object> values = record.getValue();
+                    VoucherOrder voucherOrder = BeanUtil.fillBeanWithMap(values, new VoucherOrder(), true);
+                    handleVoucherOrder(voucherOrder);
+                    // 3. send ACK. stream.orders g1 id
+                    stringRedisTemplate.opsForStream().acknowledge(queueName, "g1", record.getId());
+                } catch (Exception e) {
+                    log.error("dealing with pending list error", e);
+                    try{
+                        Thread.sleep(20);
+                    }catch (InterruptedException interruptedException){
+                        interruptedException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
     private void handleVoucherOrder(VoucherOrder voucherOrder) {
         // 1. get user
         Long userId = voucherOrder.getUserId();
@@ -89,6 +161,45 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     }
 
     private IVoucherOrderService proxy;
+
+
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        // get user
+        Long userId = UserHolder.getUser().getId();
+        Long orderId = redisIdWorker.nextId("order");
+        // 1. execute the lua script
+        Long result = stringRedisTemplate.execute(
+                SECKILL_SCRIPT,
+                Collections.emptyList(),
+                voucherId.toString(),
+                userId.toString(),
+                String.valueOf(orderId));
+        int r = result.intValue();
+        // 2. decide the lua return if it is 0
+        if (r != 0) {
+            return Result.fail(r == 1 ? "insufficient stock" : "cannot buy again");
+        }
+
+//        // 2.1. if result is not 0, cannot buy
+//        // 2.2 if result is 0, can buy, save to blocking queue
+//        VoucherOrder voucherOrder = new VoucherOrder();
+//        // 2.3 order id
+//
+//        voucherOrder.setId(orderId);
+//        // 2.4 voucher id
+//        voucherOrder.setUserId(userId);
+//        // 2.5 return order id
+//        voucherOrder.setVoucherId(voucherId);
+//        // 2.6 create blocking queue
+//        orderTasks.add(voucherOrder);
+        // 3. get proxy object
+        proxy = (IVoucherOrderService) AopContext.currentProxy();
+        return Result.ok(orderId);
+    }
+
+    /*
 
     @Override
     public Result seckillVoucher(Long voucherId) {
@@ -122,6 +233,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         proxy = (IVoucherOrderService) AopContext.currentProxy();
         return Result.ok(orderId);
     }
+
+     */
 
     //    @Override
 //    public Result seckillVoucher(Long voucherId) {
--- hm-dianping/src/main/resources/seckill.lua
+++ hm-dianping/src/main/resources/seckill.lua
@@ -3,6 +3,9 @@
 local voucherId = ARGV[1]
 -- 1.2 user id
 local userId = ARGV[2]
+-- 1.3 order id
+local orderId = ARGV[3]
+
 -- 2. data key
 -- 2.1. stock key
 local stockKey = 'seckill:stock:' .. voucherId
@@ -24,4 +27,8 @@ end
 redis.call('incrby', stockKey, -1)
 -- 3.4 put to order key
 redis.call('sadd', orderKey, userId)
+
+--3.6 send message to message queue, XADD stream.orders * k1 v1 k2 v2
+redis.call('xadd','stream.orders','*','userId',userId, 'voucherId',voucherId, 'id', orderId)
+
 return 0
